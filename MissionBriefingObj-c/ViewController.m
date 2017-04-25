e27f27d849751cf6f16c5aea55104b3260362b6f
Shane Nelson <shanenelson@shanes-macbook-pro.local>
2017-04-24 20:35:41 -0400

    MissionBriefing in OBJC

--- MissionBriefingObj-c/ViewController.m
+++ MissionBriefingObj-c/ViewController.m
@@ -6,24 +6,76 @@
 //  Copyright © 2017 Shane Nelson. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "ViewController.h" 
 
-@interface ViewController ()
+@interface ViewController <UITextFieldDelegate> ()
+@property (weak, nonatomic) IBOutlet UITextField *agentUserTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UILabel *greetingLabel;
+@property (weak, nonatomic) IBOutlet UITextView *briefingTextView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+
+- (void)viewDidLoad
+{
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+ 
+  _greetingLabel.text = @"";
+  _briefingTextView = @"";
+
 }
 
-
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)authenticateAgent:(id)sender
+{
+  
+}
+
+
+-textFieldShouldReturn:(UITextField *)textField
+
+do
+{
+  var
+  rc = NO
+  if
+  {
+    
+  }
+  do
+  {
+    if
+    {
+      textField == agentUserTextField
+    }
+    do
+    {
+      passwordTextField.becomeFirstResponder()()
+    }
+    if
+    {
+      textField == passwordTextField
+    }
+    do
+    {
+      rc = YES
+      signInAgent()
+    }
+  }
+  return rc
+}
+
+
+
+
 
 
 @end
