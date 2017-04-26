1cd3e08860565f360bd99ead194dd4df43c5f3bc
Shane Nelson <shanenelson@shanes-macbook-pro.local>
2017-04-26 16:06:50 -0400

    finished app

--- MissionBriefingObj-c/ViewController.m
+++ MissionBriefingObj-c/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h" 
 
-@interface ViewController <UITextFieldDelegate> ()
+@interface ViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *agentUserTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @property (weak, nonatomic) IBOutlet UILabel *greetingLabel;
@@ -18,64 +18,49 @@
 
 @implementation ViewController
 
-
 - (void)viewDidLoad
 {
   [super viewDidLoad];
- 
-  _greetingLabel.text = @"";
-  _briefingTextView = @"";
-
+  [self.agentUserTextField setText:@""];
+  self.greetingLabel.text = @"";
+  self.briefingTextView.text = @"";
 }
 
 - (void)didReceiveMemoryWarning
 {
   [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+ 
 }
 
-- (IBAction)authenticateAgent:(id)sender
+- (IBAction)authenticateAgent:(UIButton *)sender
 {
+ 
+  if ([self.agentUserTextField isFirstResponder])
+  {
+    [self.agentUserTextField resignFirstResponder];
+  }
   
-}
-
-
--textFieldShouldReturn:(UITextField *)textField
-
-do
-{
-  var
-  rc = NO
-  if
+  if (![self.agentUserTextField.text isEqualToString:@""] && ![self.passwordTextField.text isEqualToString:@""])
+  {
+  
+    NSString *agentName = self.agentUserTextField.text;
+    
+    NSString *lastName = [agentName componentsSeparatedByString:@" "][1];
+    self.greetingLabel.text = [NSString stringWithFormat:@"Good Evening, Agent %@", lastName];
+    
+    self.briefingTextView.text = [NSString stringWithFormat: @"This mission will be an arduous one, fraught with peril. You will cover much strange and unfamiliar territory. Should you choose to accept this mission, Agent %@, you will certainly be disavowed, but you will be doing your country a great service. This message will self destruct in 5 seconds.", lastName];
+    
+    UIColor *authenticatedBackgroundColor = [UIColor colorWithRed:0.585f green:0.78f blue:0.188f alpha:1.0f];
+    
+    self.view.backgroundColor = authenticatedBackgroundColor;
+  }
+  else
   {
     
+    UIColor *accessDeniedBackgroundColor = [UIColor colorWithRed:0.78f green:0.188f blue:0.188f alpha:1.0f];
+    
+    self.view.backgroundColor = accessDeniedBackgroundColor;
   }
-  do
-  {
-    if
-    {
-      textField == agentUserTextField
-    }
-    do
-    {
-      passwordTextField.becomeFirstResponder()()
-    }
-    if
-    {
-      textField == passwordTextField
-    }
-    do
-    {
-      rc = YES
-      signInAgent()
-    }
-  }
-  return rc
 }
-
-
-
-
-
 
 @end
